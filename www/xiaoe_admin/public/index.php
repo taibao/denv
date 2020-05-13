46c7edca6424fa964cb1ef914f2fd2fcfcd933ae
李仲元 <lizhongyuan@lizhongyuandeMacBook-Pro.local>
2020-05-13 15:25:49 +0800

    修改默认页

--- www/xiaoe_admin/public/index.php
+++ www/xiaoe_admin/public/index.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-echo '<h1 style="text-align: center;">欢迎使用DNMP！</h1>';
+echo '<h1 style="text-align: center;">欢迎使用DENV！</h1>';
 echo '<h2>版本信息</h2>';
 
 echo '<ul>';
