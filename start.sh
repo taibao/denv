6b3d4682930ec56be31e9e69087fe11d025add6e
李仲元 <lizhongyuan@lizhongyuandeMacBook-Pro.local>
2020-05-14 19:49:07 +0800

    更新多文件配置

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 set -e
 root=$(pwd)
 port=80
+ran=$RANDOM
 while getopts ":v:p:c" opt
 do
     case $opt in
@@ -13,13 +14,13 @@ do
 done
 
 #echo $path
-sed -i '' '11 a \
+sed -i '' '10 a \
 \ \ \ \ \ \ \'$'- ''"'$port:$port'"''\'$'\n ' $root/docker-compose.yml
 sed -i '' '8 a \
 EXPOSE '$port'\'$'\n ' $root/services/nginx/Dockerfile
 
-cp $root/services/nginx/conf.d/localhost.conf $root/services/nginx/conf.d/$path.conf
+cp $root/services/nginx/conf.d/localhost.conf $root/services/nginx/conf.d/$path$ran.conf
 sed -i '' '2,3c \
 \ \ \ \ listen '$port'; \
-\ \ \ \ root /www/'$path'/public;\'$'\n' $root/services/nginx/conf.d/$path.conf
+\ \ \ \ root /www/'$path'/public;\'$'\n' $root/services/nginx/conf.d/$path$ran.conf
 docker-compose up -d --build
